9f7e5825e4904768fd31febc4ed6860f06357470
Jessé Orestes <jesseorestespereira1@gmail.com>
2021-09-27 20:22:52 -0300

    init project

---
+++ paesdajo/src/main/java/com/darwinsoft/paesdajo/PaesdajoApplication.java
@@ -0,0 +1,13 @@
+package com.darwinsoft.paesdajo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaesdajoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaesdajoApplication.class, args);
+	}
+
+}
---
+++ paesdajo/src/test/java/com/darwinsoft/paesdajo/PaesdajoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darwinsoft.paesdajo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaesdajoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
